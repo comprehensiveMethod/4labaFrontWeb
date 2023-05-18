ab4ce30e967385614f07f7135c8222655319ad66
ComprehensiveMethod <timur32240@gmail.com>
2023-05-19 00:06:37 +0700

    added all

---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="footer-content">
+          <div class="logo-home"></div>
+          <div class="footer-text">
+
+
+            <p>© 2023 Timurmarch. Все права защищены невероятным способом.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  min-height: 300px;
+  text-align: center;
+  background-color: #111222;
+  padding-top: 120px;
+  font-size: 16px;
+  font-weight: 200;
+  color: #969696;
+  padding-right: 20px;
+  padding-bottom: 20px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
